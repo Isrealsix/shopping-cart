b251498279dedc1d3b0f79a4a5e0ae380d9a6501
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-27 00:50:47 +0700

    added model and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import { Wrapper } from "./App.styles";
+import { ICartItem } from "./model";
+
+const getProducts = async (): Promise<ICartItem[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
 const App = () => {
-  return (
-    <div>Start</div>
-  )
-}
+  const { data, isLoading, error } = useQuery<ICartItem[]>('products', getProducts);
+
+  const getTotalItems = () => null;
+  const handleAddToCart = () => null;
+  const handleRemoveFromCart = () => null;
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong</div>
+  return <div>Start</div>;
+};
 
 export default App;
