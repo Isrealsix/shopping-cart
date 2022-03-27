9f84df05accff8659db4a976d7147fa6855cf98f
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-27 12:13:28 +0700

    added functionality to display cart Items

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AddShoppingCart } from "@material-ui/icons";
 import { Wrapper, StyledButton } from "./App.styles";
 import { ICartItem } from "./model";
 import Item from "./components/Item/Item";
+import Cart from "./components/Cart/Cart";
 
 const getProducts = async (): Promise<ICartItem[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
@@ -12,11 +13,14 @@ const getProducts = async (): Promise<ICartItem[]> =>
 const App = () => {
   const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
   const [cartItems, setCartItems] = useState([] as ICartItem[]);
-  const { data, isLoading, error } = useQuery<ICartItem[]>('products', getProducts);
+  const { data, isLoading, error } = useQuery<ICartItem[]>(
+    "products",
+    getProducts
+  );
 
-  const getTotalItems = (items: ICartItem[]) => 
+  const getTotalItems = (items: ICartItem[]) =>
     items.reduce((acc, item) => acc + item.amount, 0);
-  
+
   const handleAddToCart = () => null;
   const handleRemoveFromCart = () => null;
 
@@ -24,15 +28,23 @@ const App = () => {
   if (error) return <div>Something went wrong</div>;
   return (
     <Wrapper>
-      <Drawer anchor="right" open={cartIsOpen} onClose={() => setCartIsOpen(false)}>
-        cart items dsplays here
+      <Drawer
+        anchor="right"
+        open={cartIsOpen}
+        onClose={() => setCartIsOpen(false)}
+      >
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartIsOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error"></Badge>
         <AddShoppingCart />
       </StyledButton>
       <Grid container spacing={3}>
-        {data?.map(item => (
+        {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
             <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
